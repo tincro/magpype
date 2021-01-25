fb522bf52f976d9775446a128dd6610698419dcc
Austin Cronin <austin.cronin3d@gmail.com>
2021-01-25 16:18:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="magpie", # Replace with your own username
+    name="magpype", 
     version="0.0.1",
     author="Austin Cronin",
     author_email="austin.cronin3d@gmail.com",
     description="A package for project creation and navigation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/tincro/magpie",
+    url="https://github.com/tincro/magpype",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
