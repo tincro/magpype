0482b355823469b9dc6cc0cb53f6c08a3708cb03
Austin Cronin <austin.cronin3d@gmail.com>
2021-01-28 22:20:24 -0500

    updated version in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="magpype", 
-    version="0.0.1",
+    version="1.0.0",
     author="Austin Cronin",
     author_email="austin.cronin3d@gmail.com",
     description="A package for project creation and navigation",
